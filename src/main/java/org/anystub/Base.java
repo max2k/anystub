c4e9a0a7f347c68cf949d15a17a237f315f6bcc3
feech <andreev.kirill.o@gmail.com>
2021-11-13 15:55:55 +0000

    sonar happier

--- src/main/java/org/anystub/Base.java
+++ src/main/java/org/anystub/Base.java
@@ -234,7 +234,7 @@ public class Base {
             try {
                 kk[i] = objectMapper.writeValueAsString(keys[i]);
             } catch (JsonProcessingException e) {
-                throw new RuntimeException(e);
+                throw new TypeNotPresentException(keys[i].getClass().getName(), e);
             }
         }
 
@@ -244,7 +244,7 @@ public class Base {
                     try {
                         r = objectMapper.readValue(values, responseClass);
                     } catch (JsonProcessingException e) {
-                        throw new RuntimeException(e);
+                        throw new TypeNotPresentException(keys[i].getClass().getName(), e);
                     }
                     return r;
                 },
