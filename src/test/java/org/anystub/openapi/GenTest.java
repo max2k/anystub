2a33cb46aac5c613d47dc601a0825f29e0a92e8d
feech <andreev.kirill.o@gmail.com>
2021-11-15 01:13:37 +0000

    fixed template

--- src/test/java/org/anystub/openapi/GenTest.java
+++ src/test/java/org/anystub/openapi/GenTest.java
@@ -1,4 +1,27 @@
 package org.anystub.openapi;
 
+import org.anystub.AnyStubId;
+import org.junit.jupiter.api.Test;
+import org.openapitools.client.ApiClient;
+import org.openapitools.client.ApiException;
+import org.openapitools.client.api.PetApi;
+import org.openapitools.client.api.PetApiTest;
+import org.openapitools.client.model.Pet;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class GenTest {
+
+    @Test
+    @AnyStubId
+    void requestGen() throws ApiException {
+        PetApi petApi = new PetApi();
+
+        PetApiTest petApiTest = new PetApiTest(petApi);
+
+        Pet petById = petApiTest.getPetById(123L);
+
+
+        assertEquals(1L, petById.getId());
+    }
 }
